9cb2730a25862f50788a50aecedcc594079c8b05
charlescyso <charlescyso@gmail.com>
2022-09-25 21:49:44 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { DOM_CONTENT } from './scripts/initialDOM.js';
 import { DOM_EVENTS } from './scripts/DOMEvents.js';
 
+import './styles/normalize.css'
 import './styles/style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
